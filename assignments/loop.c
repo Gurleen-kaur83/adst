4070f37f53847fc88fa77c69f969d14eafcf01be
Gurleen <kaur.gurleen@gobindsarvar.ca>
2025-03-12 18:01:02 +0000

    Loops

--- assignments/loop.c
+++ assignments/loop.c
@@ -5,8 +5,24 @@ int main () {
     Write a C program to print numbers from 0 to 10 and 10 to 0
     using two while loops.
    */
-  for (int i = 0; i<= 10; i= i+=2){
-    printf("The value of i is %d\n",i);
+  int i = 0;
+
+while (i < 5) {
+  printf("%d\n", i);
+  i++;
+}
+
+int a = 0;
+
+do {
+  printf("%d\n", a);
+  a++;
+}
+while (a < 5);
+
+
+  for (int i = 1; i <= 10; i++) {
+    printf("The value of i is %d\n", i);
   }
 
   return 0;
