050e271154b63252fadc09842f7ba0b09acd310d
Gurleen <kaur.gurleen@gobindsarvar.ca>
2025-03-13 18:08:02 +0000

    changes

--- assignments/arrays.c
+++ assignments/arrays.c
@@ -16,12 +16,11 @@ int main() {
     int ages[] = {20,22,18,35,48,26,87,70};
     float avg;
     int sum=0;
-    int i;
 
     int length = sizeof(ages) / sizeof(ages[0]);
 
     // Loop 
-    for (i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++) {
     sum += ages[i];
     // sum=sum+ages[i]
 
