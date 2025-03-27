c831785de26b2aebb6aea95dc3a996f92e1171f9
Gurleen <kaur.gurleen@gobindsarvar.ca>
2025-03-27 01:38:58 +0000

    Conditions

--- assignments/conditions.c
+++ assignments/conditions.c
@@ -10,22 +10,14 @@ int main() {
 
     Please use if conditions.
      */
-//     int age = 16;
 
-    int age = 21;
+   int agev = 17;
 
-    if(age >= 18) {
-      printf("Congratulations! You are eligible for casting your vote.");
-    } else {
-      printf("Sorry, you cannot vote!");
-    }
-   // int age = 15;
-
-//    if (age >= 18) {
-//        printf("Congratulations! You are eligible for casting your vote.\n");
-//    } else {
-//        printf("Sorry! You are not eligible to vote yet.\n");
-//    }
+   if (agev >= 18) {
+       printf("Congratulations! You are eligible for casting your vote.\n");
+   } else {
+       printf("Sorry! You are not eligible to vote yet.\n");
+   }
 
 
     int age = 16;
@@ -36,21 +28,6 @@ int main() {
         } else {
         printf("Sorry! You cannot get a license, yet\n");
       }
-   // if(age >= 18) {
-   //     printf("Congratulation! You are eligible for casting your vote.\n");
-   // } else {
-   //     printf("Sorry! You are not eligible for casting your vote.\n");
-   // }
 
-
-   int age = 15;
-   if (age >= 16 && age < 18) {
-    printf("You can get a learner's license\n");
-   } else if (age>= 18) {
-    printf("You can get a class 5 license\n");
-   } else {
-    printf ("Sorry! You can not get a license, yet \n");
-
-   }
     return 0;
 }
